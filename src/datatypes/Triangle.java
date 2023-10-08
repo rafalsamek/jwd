c38e7ed7cd891fb11a69c113397b5d97bf8aec94
Rafal Samek <rsamek@datto.com>
2023-10-08 19:54:54 +0200

    TriangleApplication.jave minor changes

--- src/datatypes/Triangle.java
+++ src/datatypes/Triangle.java
@@ -1,12 +1,13 @@
 package datatypes;
 
 public class Triangle {
-    char sign;
+    char sign = '©';
+    String space = " ";
 
     public void drawTriangle() {
-        System.out.println("   " + sign + "   ");
-        System.out.println("  " + sign  + " " +  sign + "  ");
-        System.out.println(" " + sign + "   " + sign + " ");
-        System.out.println(sign + " " + sign + " " + sign + " " + sign);
+        System.out.println(space + space + space + sign + space + space + space);
+        System.out.println(space + space + sign  + space +  sign + space + space);
+        System.out.println(space + sign + space + space + space + sign + space);
+        System.out.println(sign + space + sign + space + sign + space + sign);
     }
 }
