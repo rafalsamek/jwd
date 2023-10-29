b4cbe680a0e19c06690d6196ea989c5eb4d1a35f
Rafal Samek <rsamek@datto.com>
2023-10-29 19:40:42 +0100

    homeworks.homework1: pkt 2.: Calculator

---
+++ src/homeworks/homework1/Calculator.java
@@ -0,0 +1,29 @@
+package homeworks.homework1;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int a = 7;
+        int b = 34;
+
+        System.out.println(a + " + " + b + " = " + add(a, b));
+        System.out.println( a + " - " + b + " = " + subtract(a, b));
+        System.out.println(a + " * " + b + " = " + multiply(a, b));
+        System.out.println(a + " / " + b + " = " + divide(a, b));
+    }
+
+    private static int add(int a, int b) {
+        return  a + b;
+    }
+
+    private static int subtract(int a, int b) {
+        return  a - b;
+    }
+
+    private static  int multiply(int a, int b) {
+        return  a * b;
+    }
+
+    private static int divide(int a, int b) {
+        return  a / b;
+    }
+}
