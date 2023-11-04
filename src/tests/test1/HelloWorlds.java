5a03a8d424fae80d27a02987d9b221b824a0feec
rsamek <rafal.samek@gmail.com>
2023-11-04 10:54:34 +0100

    tests.test1

---
+++ src/tests/test1/HelloWorlds.java
@@ -0,0 +1,15 @@
+package tests.test1;
+
+/**
+ * Napisz program wyświetlający na ekranie 5 napisów Witaj świecie!
+ */
+
+public class HelloWorlds {
+    public static void main(String[] args) {
+        System.out.println("Witaj świecie!");
+        System.out.println("Witaj świecie!");
+        System.out.println("Witaj świecie!");
+        System.out.println("Witaj świecie!");
+        System.out.println("Witaj świecie!");
+    }
+}
