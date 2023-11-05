f275419569d72b71eef30706c29d1b058829f7d7
rsamek <rafal.samek@gmail.com>
2023-11-05 11:39:57 +0100

    arrays

--- src/arrays/Demo.java
+++ src/arrays/Demo.java
@@ -47,5 +47,32 @@ public class Demo {
         System.out.println(ppoints[0]); // 50, 1
         System.out.println(ppoints[1]); // 50, 1
         System.out.println(ppoints[2]); // 50, 1
+
+        int[] arr = {1, 2, 3, 5, 7, 12, 45};
+
+        for (int i = 0; i < arr.length ; i++) {
+            System.out.println("i: " + i + " ma wartość: " + arr[i]);
+        }
+        System.out.println();
+
+        for (int number: arr) {
+            System.out.println(number);
+        }
+
+        // tablice wielowymiarowe (tablice tablic)
+        String[] dogs = {"Burek", "Pluto", "Łajka"};
+        String[] people = {"Ania", "Wojtek", "Dorota"};
+
+        String[][] names = {dogs, people};
+
+        for (int i = 0; i < names.length; i++) {
+            for (int j = 0; j < names[i].length; j++) {
+                System.out.print(names[i][j]);
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+        int[][][] multi = {{{1}},{{1}, {2}}};
     }
 }
