d26c267f92e22822bfed549ed79d79bb1deb7dce
Rafal Samek <rsamek@datto.com>
2023-11-12 02:18:14 +0100

    tools/ColorConverter.java tools/HexConverter.java

---
+++ src/tools/ColorConverter.java
@@ -0,0 +1,41 @@
+package tools;
+
+public class ColorConverter {
+    public static void main(String[] args) {
+        String color  = "#ce6d28";
+        System.out.println("Before conversion: " + color);
+
+        int[] rgb = ColorConverter.colorHexToRgb(color);
+        System.out.println("After conversion hex to rgb: ");
+        System.out.println("red: " + rgb[0]);
+        System.out.println("green: " + rgb[1]);
+        System.out.println("blue: " + rgb[2]);
+
+        String hex = ColorConverter.colorRgbToHex(
+            rgb[0],
+            rgb[1],
+            rgb[2]
+        );
+
+        System.out.println("After conversion rgb to hex: ");
+        System.out.println(hex);
+    }
+
+    public static int[] colorHexToRgb(String color) {
+        int[] rgb = new int[3];
+        rgb[0] = HexConverter.hexToDecimal(color.substring(1, 3));
+        rgb[1] = HexConverter.hexToDecimal2(color.substring(3, 5));
+        rgb[2] = HexConverter.hexToDecimal(color.substring(5, 7));
+
+        return rgb;
+    }
+
+    public static String colorRgbToHex(int red, int green, int blue) {
+        String hex = "#";
+        hex += HexConverter.decimalToHex(red);
+        hex += HexConverter.decimalToHex(green);
+        hex += HexConverter.decimalToHex(blue);
+
+        return hex;
+    }
+}
---
+++ src/tools/HexConverter.java
@@ -0,0 +1,40 @@
+package tools;
+
+public class HexConverter {
+    public static int hexToDecimal(String hex) throws IllegalStateException {
+        int sum = 0;
+        int w = hex.length();
+        for (int i = 0; i < hex.length(); i++) {
+            w--;
+            int digit = switch(hex.charAt(i)) {
+                case '0' -> 0;
+                case '1' -> 1;
+                case '2' -> 2;
+                case '3' -> 3;
+                case '4' -> 4;
+                case '5' -> 5;
+                case '6' -> 6;
+                case '7' -> 7;
+                case '8' -> 8;
+                case '9' -> 9;
+                case 'a' -> 10;
+                case 'b' -> 11;
+                case 'c' -> 12;
+                case 'd' -> 13;
+                case 'e' -> 14;
+                case 'f' -> 15;
+                default -> throw new IllegalStateException("Unexpected value: " + hex.charAt(i));
+            };
+            sum += digit * Math.pow(16, w);
+        }
+        return sum;
+    }
+
+    public static int hexToDecimal2(String hex) {
+        return Integer.parseInt(hex, 16);
+    }
+
+    public static String decimalToHex(int decimal) {
+        return Integer.toHexString(decimal);
+    }
+}
