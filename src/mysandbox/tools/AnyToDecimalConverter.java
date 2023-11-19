3b360a94a7dc57e4e67c698d96b783972ba57d25
Rafal Samek <rsamek@datto.com>
2023-11-19 05:28:32 +0100

    mysandbox/tools/AnyToDecimalConverter - convertDigit() added

--- src/mysandbox/tools/AnyToDecimalConverter.java
+++ src/mysandbox/tools/AnyToDecimalConverter.java
@@ -27,7 +27,7 @@ public class AnyToDecimalConverter {
 
             int decimal;
             try {
-                decimal = AnyToDecimalConverter.convert(number, numberSystem);
+                decimal = AnyToDecimalConverter.convertNumber(number, numberSystem);
             } catch (Throwable throwable) {
                 System.out.println(throwable.getMessage());
                 System.out.println();
@@ -44,7 +44,7 @@ public class AnyToDecimalConverter {
         System.out.println("Goodbye!");
     }
 
-    public static int convert(String any, int numberSystem) throws IllegalStateException {
+    public static int convertNumber(String any, int numberSystem) throws IllegalStateException {
         if (numberSystem > 36 || numberSystem < 2) {
             throw new IllegalStateException("Number system range is from 2 to 36.");
         }
@@ -52,53 +52,58 @@ public class AnyToDecimalConverter {
         int weight = any.length();
         for (int i = 0; i < any.length(); i++) {
             weight--;
-            int digit = switch(any.charAt(i)) {
-                case '0' -> 0;
-                case '1' -> 1;
-                case '2' -> 2;
-                case '3' -> 3;
-                case '4' -> 4;
-                case '5' -> 5;
-                case '6' -> 6;
-                case '7' -> 7;
-                case '8' -> 8;
-                case '9' -> 9;
-                case 'a', 'A' -> 10;
-                case 'b', 'B' -> 11;
-                case 'c', 'C' -> 12;
-                case 'd', 'D' -> 13;
-                case 'e', 'E' -> 14;
-                case 'f', 'F' -> 15;
-                case 'g', 'G' -> 16;
-                case 'h', 'H' -> 17;
-                case 'i', 'I' -> 18;
-                case 'j', 'J' -> 19;
-                case 'k', 'K' -> 20;
-                case 'l', 'L' -> 21;
-                case 'm', 'M' -> 22;
-                case 'n', 'N' -> 23;
-                case 'o', 'O' -> 24;
-                case 'p', 'P' -> 25;
-                case 'q', 'Q' -> 26;
-                case 'r', 'R' -> 27;
-                case 's', 'S' -> 28;
-                case 't', 'T' -> 29;
-                case 'u', 'U' -> 30;
-                case 'v', 'V' -> 31;
-                case 'w', 'W' -> 32;
-                case 'x', 'X' -> 33;
-                case 'y', 'Y' -> 34;
-                case 'z', 'Z' -> 35;
-                default -> throw new IllegalStateException("Unexpected value: " + any.charAt(i));
-            };
-
-            if (digit >= numberSystem) {
-                throw new IllegalStateException("The digit: " + any.charAt(i) + " is out of range of " + numberSystem + " number system.");
-            }
-
+            int digit = AnyToDecimalConverter.convertDigit(any.charAt(i), numberSystem);
             sum += digit * Math.pow(numberSystem, weight);
         }
 
         return sum;
+    }
+
+    public static int convertDigit(char digit, int numberSystem) {
+        int result = switch(digit) {
+            case '0' -> 0;
+            case '1' -> 1;
+            case '2' -> 2;
+            case '3' -> 3;
+            case '4' -> 4;
+            case '5' -> 5;
+            case '6' -> 6;
+            case '7' -> 7;
+            case '8' -> 8;
+            case '9' -> 9;
+            case 'a', 'A' -> 10;
+            case 'b', 'B' -> 11;
+            case 'c', 'C' -> 12;
+            case 'd', 'D' -> 13;
+            case 'e', 'E' -> 14;
+            case 'f', 'F' -> 15;
+            case 'g', 'G' -> 16;
+            case 'h', 'H' -> 17;
+            case 'i', 'I' -> 18;
+            case 'j', 'J' -> 19;
+            case 'k', 'K' -> 20;
+            case 'l', 'L' -> 21;
+            case 'm', 'M' -> 22;
+            case 'n', 'N' -> 23;
+            case 'o', 'O' -> 24;
+            case 'p', 'P' -> 25;
+            case 'q', 'Q' -> 26;
+            case 'r', 'R' -> 27;
+            case 's', 'S' -> 28;
+            case 't', 'T' -> 29;
+            case 'u', 'U' -> 30;
+            case 'v', 'V' -> 31;
+            case 'w', 'W' -> 32;
+            case 'x', 'X' -> 33;
+            case 'y', 'Y' -> 34;
+            case 'z', 'Z' -> 35;
+            default -> throw new IllegalStateException("Unexpected value: " + digit);
+        };
+
+        if (result >= numberSystem) {
+            throw new IllegalStateException("The digit: " + digit + " is out of range of " + numberSystem + " number system.");
+        }
+
+        return result;
     }
 }
