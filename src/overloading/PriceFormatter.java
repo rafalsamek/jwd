9b5cfbc09994aab7467ff2ae47310754fc4a589f
rsamek <rafal.samek@gmail.com>
2023-12-09 10:02:48 +0100

    overloading/PriceFormatter

---
+++ src/overloading/PriceFormatter.java
@@ -0,0 +1,24 @@
+package overloading;
+
+public class PriceFormatter {
+    String currency = "zł";
+
+    private String formatPrice(int value) {
+        return String.format("%d,00 %s", value, currency);
+    }
+
+    private String formatPrice(double value) {
+        return String.format("%.2f %s", value, currency);
+    }
+
+    private String formatPrice(String value) {
+        return String.format("%.2f %s", Double.parseDouble(value), currency);
+    }
+
+    public static void main(String[] args) {
+        PriceFormatter pf = new PriceFormatter();
+        System.out.println(pf.formatPrice(500));
+        System.out.println(pf.formatPrice(89.9934));
+        System.out.println(pf.formatPrice("550"));
+    }
+}
