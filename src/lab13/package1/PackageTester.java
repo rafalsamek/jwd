7dc76216bd8c98d58ad43c5473c5e5d211531c94
rsamek <rafal.samek@gmail.com>
2023-12-10 11:08:17 +0100

    packages lab13

---
+++ src/lab13/package1/PackageTester.java
@@ -0,0 +1,7 @@
+package lab13.package1;
+
+public class PackageTester {
+    public void introduce() {
+        System.out.println("Nazwa pakietu: " + this.getClass().getPackageName());
+    }
+}
---
+++ src/lab13/package3/PackageTester.java
@@ -0,0 +1,7 @@
+package lab13.package3;
+
+public class PackageTester {
+    public void introduce() {
+        System.out.println("Nazwa pakietu: " + this.getClass().getPackageName());
+    }
+}
