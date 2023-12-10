e18573c30281ac107e3fd9142241aeced2022656
rsamek <rafal.samek@gmail.com>
2023-12-10 10:52:53 +0100

    packages

--- src/packages/StaticImporter.java
+++ src/packages/StaticImporter.java
@@ -3,6 +3,9 @@ package packages;
 import static java.lang.Math.PI;
 import static java.lang.Math.sqrt;
 
+import java.awt.*; //nie importuje podpakietów
+// import java.awt.font.*; np. to trzeba osobno zaimportować
+
 public class StaticImporter {
     public static void main(String[] args) {
         System.out.println(java.lang.Math.sqrt(25));
