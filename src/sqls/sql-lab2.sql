75bdb7932ddec290341f690ede6c53790ab5aa3e
Rafal Samek <rsamek@datto.com>
2023-12-13 05:40:03 +0100

    sqls

--- src/sqls/sql-lab2.sql
+++ src/sqls/sql-lab2.sql
@@ -11,40 +11,40 @@ GO
 DROP TABLE IF EXISTS Topics
 GO
 
-CREATE TABLE Topics (
-    TopicID int IDENTITY,
-)
-GO
+-- CREATE TABLE Topics (
+--     TopicID int IDENTITY
+-- )
+-- GO
 
 DROP TABLE IF EXISTS Clues
 GO
 
-CREATE TABLE Clues (
-
-)
-GO
+-- CREATE TABLE Clues (
+--     ClueID int IDENTITY
+-- )
+-- GO
 
 DROP TABLE IF EXISTS Users
 GO
 
-CREATE TABLE Users (
-
-)
-GO
+-- CREATE TABLE Users (
+--     UserID int IDENTITY
+-- )
+-- GO
 
 DROP TABLE IF EXISTS UsersLogins
 GO
 
-CREATE TABLE UsersLogins (
-
-)
-GO
+-- CREATE TABLE UsersLogins (
+--     UserLoginID int IDENTITY
+-- )
+-- GO
 
 DROP TABLE IF EXISTS UserStats
 GO
 
-CREATE TABLE UserStats (
-
-)
-GO
+-- CREATE TABLE UserStats (
+--    UserStatID int IDENTITY
+-- )
+-- GO
 
