3b60aa45fc1aae19bed94e0b8d5343e8eb085c18
rsamek <rafal.samek@gmail.com>
2024-02-17 14:15:54 +0100

    exceptions

---
+++ src/exceptions/StackTraceExample.java
@@ -0,0 +1,20 @@
+package exceptions;
+
+public class StackTraceExample {
+    public void method1() {
+        method2();
+    }
+
+    public void method2() {
+        method3();
+    }
+
+    public void method3() {
+        throw new RuntimeException("Bum!");
+    }
+
+    public static void main(String[] args) {
+        StackTraceExample example = new StackTraceExample();
+        example.method1();
+    }
+}
