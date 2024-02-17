c6de69e9156b780d3ee81d9ca9d8cba8509096a8
rsamek <rafal.samek@gmail.com>
2024-02-17 16:28:58 +0100

    enums

---
+++ src/enums/FormatterTester.java
@@ -0,0 +1,8 @@
+package enums;
+
+public class FormatterTester {
+    public static void main(String[] args) {
+        System.out.println(Formatter.CALM.format("Hi, how are you?"));
+        System.out.println(Formatter.NERVOUS.format("Hi, how are you getting on?"));
+    }
+}
