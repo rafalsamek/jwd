9e7781981947c3b73f91037c4a67c765776ab7d6
rsamek <rafal.samek@gmail.com>
2024-02-18 14:41:32 +0100

    database

--- src/database/MyFirstJdbcConnection.java
+++ src/database/MyFirstJdbcConnection.java
@@ -1,14 +1,25 @@
 package database;
 
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
+/**
+ * 10 pierwszych imion i nazwisk osób z tabeli Contact bazy AdventureWorks
+ * SELECT TOP 10 * FROM Person.Contact
+ */
 public class MyFirstJdbcConnection {
     public static void main(String[] args) {
-        String connectionUrl = "jdbc:sqlserver://localhost;encrypt=true;user=MyUserName;password=*****;";
+        String connectionUrl = "jdbc:sqlserver://morfeusz.wszib.edu.pl;encrypt=true;trustServerCertificate=true;databaseName=AdventureWorks;user=rsamek;password=***;";
+        String sql = "SELECT TOP 10 * FROM Person.Contact";
 
-        try {
-            DriverManager.getConnection(connectionUrl);
+        try(Connection con = DriverManager.getConnection(connectionUrl);
+            Statement stmt = con.createStatement()) {
+
+            ResultSet rs = stmt.executeQuery(sql);
+
+            while (rs.next()) {
+                System.out.println(rs.getString("FirstName") + " " + rs.getString("LastName"));
+            }
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
