1977fb7de9e093ce32e5721145c326ea8c5b301f
Rafal Samek <rsamek@datto.com>
2024-07-14 09:28:55 +0200

    lambda

--- src/lambda/MyFirstLambda.java
+++ src/lambda/MyFirstLambda.java
@@ -5,6 +5,9 @@ package lambda;
 // (Integer x, Long y) -> System.out.println(x * y)
 // (Integer x) -> {if (x > 2) return x else return 0);
 
+import java.util.function.Consumer;
+import java.util.function.UnaryOperator;
+
 public class MyFirstLambda {
     public static void main(String[] args) {
         Multiplier multiplier = x -> x * x;
@@ -14,13 +17,24 @@ public class MyFirstLambda {
 
         Introducer introducer = name -> System.out.println("Cześć! Mam na imię " + name + ".");
         introducer.run("Rafał");
+
+        System.out.println();
+
+        // korzystamy z gotowych interfaców funkcyjnych
+        UnaryOperator<Integer> uo = o -> o * o;
+        System.out.println(uo.apply(2));
+
+        Consumer<String> consumer = name -> System.out.println("Cześć! Mam na imię " + name + ".");
+        consumer.accept("Rafał");
     }
 }
 
+@FunctionalInterface
 interface Multiplier {
     int perform(int x);
 }
 
+@FunctionalInterface
 interface Introducer {
     void run(String name);
 }
