d1cae6464daeb029f90fea8f8cc68576374b5475
Rafal Samek <rsamek@datto.com>
2024-07-14 09:48:55 +0200

    lambda

--- src/lambda/MethodReferenceOperatorDemo.java
+++ src/lambda/MethodReferenceOperatorDemo.java
@@ -4,7 +4,7 @@ import java.util.function.Function;
 
 public class MethodReferenceOperatorDemo {
     public static void main(String[] args) {
-        Function<String, Integer> function = s -> Integer.parseInt(s);
+        Function<String, Integer> function = Integer::parseInt;
         int number = function.apply("123");
     }
 }
