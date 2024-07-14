c0e049b2466fefac8bc8ebe035c15f2ae735c9a0
Rafal Samek <rsamek@datto.com>
2024-07-14 09:40:17 +0200

    lambda

--- src/lambda/PlanetsLambda.java
+++ src/lambda/PlanetsLambda.java
@@ -16,13 +16,7 @@ public class PlanetsLambda {
                 "Neptun"
         };
 
-        Comparator<String> comparator = new Comparator<String>() {
-
-            @Override
-            public int compare(String o1, String o2) {
-                return o1.length() - o2.length();
-            }
-        };
+        Comparator<String> comparator = (o1, o2) -> o1.length() - o2.length();
 
         Arrays.sort(planets, comparator);
 
